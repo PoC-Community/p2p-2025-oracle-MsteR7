78e3755854455db8fd528313024477a34fa0ae39
MsteR7 <ester.ngindu-mubiala@epitech.eu>
2025-12-17 16:23:48 +0100

    feat: step 3

--- utils/Oracle.sol
+++ utils/Oracle.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import {Script} from "forge-std/Script.sol";
-import {Oracle} from "../src/Oracle.sol";
+import {Oracle} from "./src/Oracle.sol";
 
 contract OracleScript is Script {
     Oracle public oracle;
